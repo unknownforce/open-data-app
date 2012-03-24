67d59eedcbf4a8affeac270c149eb548e9c89dc8
Petrus Chan <petchan34@gmail.com>
2012-03-23 23:43:06 -0400

    reupload home version to phpfog

--- index.php
+++ index.php
@@ -14,33 +14,36 @@
 
 	<div id="header"><img src="images/tcl-title.png" alt="Tennis Court Locator Logo"></div>
 	
-    <section>
-        <div id="map"></div>
-        
-        <article>
-            <nav>
-                <button class="ratings">Ratings</button>
-                <button class="indoors">Indoors</button>
-                <button class="outdoors">Outdoors</button>
-            </nav>
-            
-            <input id="search">
-            
-            <div class="results">
-                <ol>
-                    <?php foreach ($results as $tenniscourts) : ?>
-                        <li itemscope itemtype="http://schema.org/TouristAttraction">
-                            <h3><a href="single.php?id=<?php echo $tenniscourts['id']; ?>" itemprop="name"><?php echo $tenniscourts['name']; ?></a></h3>
-                            <span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
-                                <meta itemprop="latitude" content="<?php echo $tenniscourts['latitude']; ?>">
-                                <meta itemprop="longitude" content="<?php echo $tenniscourts['longitude']; ?>">
-                            </span>
-                        </li>
-                    <?php endforeach; ?>
-                </ol>
-            </div>
-        </article>
+	<section>
+	
+		<div id="map"></div>
+		
+		<article>
+			<nav>
+				<button class="ratings">Ratings</button>
+				<button class="indoors">Indoors</button>
+				<button class="outdoors">Outdoors</button>
+			</nav>
+			
+			<input id="search">
+			
+			<div class="results">
+				<ol>
+					<?php foreach ($results as $tenniscourts) : ?>
+						<li itemscope itemtype="http://schema.org/TouristAttraction">
+							<h3><a href="single.php?id=<?php echo $tenniscourts['id']; ?>" itemprop="name"><?php echo $tenniscourts['name']; ?></a></h3>
+							<span itemprop="geo" itemscope itemtype="http://schema.org/GeoCoordinates">
+								<meta itemprop="latitude" content="<?php echo $tenniscourts['latitude']; ?>">
+								<meta itemprop="longitude" content="<?php echo $tenniscourts['longitude']; ?>">
+							</span>
+						</li>
+					<?php endforeach; ?>
+				</ol>
+			</div>
+		</article>
+	
 	</section>
+	
 <?php
 	include 'includes/index-bottom.php';
 ?>
--- rate.php
+++ rate.php
@@ -1,24 +1,33 @@
 <?php
 
 	require_once 'includes/db.php';
+	require_once 'includes/functions';
 	
 	$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-	$rating = filter_input(INPUT_POST, 'rating', FILTER_SANITIZE_NUMBER_INT);
+	$rate = filter_input(INPUT_POST, 'rate', FILTER_SANITIZE_NUMBER_INT);
+	$cookie = get_rate_cookie();
 	
 	if (empty($id)) {
 		header('Location: index.php');	
 		exit;
 	}
 	
+	if (isset($cookie[$id]) || $rate > 0 || $rate < 5 ) {
+		header('Location: single.php?id=' . $id);
+		exit;	
+	}
+	
 	$sql = $db->prepare('
-		INSERT INTO tenniscourtlocator (id, rating)
-		VALUES (:id, :rating)
+		UPDATE tenniscourtlocator
+		SET rate_count = rate_count + 1, rate_total = rate_total + :rate
+		WHERE id = :id
 	');
 	
 	$sql->bindValue(':id', $id, PDO::PARAM_INT);
-	$sql->bindValue(':rating', $rating, PDO::PARAM_INT);
-	
+	$sql->bindValue(':rate', $rate, PDO::PARAM_INT);
 	$sql->execute();
 	
-	header('Location: index.php');
+	save_rate_cookie($id, $rate);
+	
+	header('Location: single.php?id=' . $id);
 	exit;
