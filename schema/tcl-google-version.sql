79ce45a4b10e88a3fbf975c163ffcfd3649ada46
unknownforce <petchan34@gmail.com>
2012-04-09 13:37:53 -0400

    exported new version of SQL database, with the inclusion of outdoor and indoor locations

--- schema/tcl-google-version.sql
+++ schema/tcl-google-version.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 30, 2012 at 04:44 PM
+-- Generation Time: Apr 09, 2012 at 05:19 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -36,19 +36,22 @@ CREATE TABLE IF NOT EXISTS `tenniscourtlocator` (
   `isindoor` tinyint(1) NOT NULL,
   `isoutdoor` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `tenniscourtlocator`
 --
 
 INSERT INTO `tenniscourtlocator` (`id`, `name`, `longitude`, `latitude`, `street_address`, `rate_count`, `rate_total`, `isindoor`, `isoutdoor`) VALUES
-(1, 'St. James Tennis Club', -75.691462, 45.401809, '183 Third Ave.', 0, 0, 0, 0),
-(2, 'Ottawa Tennis & Lawn Bowling Club', -75.687491, 45.388577, '176 Cameron Ave.', 0, 0, 0, 0),
-(3, 'Rideau Tennis Club', -75.669499, 45.426854, '1 Donald Street', 0, 0, 0, 0),
-(4, 'Ottawa New Edinburgh Club', -75.685965, 45.450308, '504 Rockcliffe Driveway', 0, 0, 0, 0),
-(5, 'Tennis Center West Ottawa', -75.797775, 45.362787, '120 Greenview Ave.', 0, 0, 0, 0),
-(6, 'Carleton Tennis Centre', -75.697517, 45.383532, '1125 Colonel By Dr.', 0, 0, 0, 0),
-(7, 'Dara Tennis Club', -75.548741, 45.288961, '49-A Maple Dr.', 0, 0, 0, 0),
-(8, 'Super Dome', -75.575414, 45.442983, '1660 Bearbrook Rd.', 0, 0, 0, 0),
-(9, 'Elmdale Tennis', -75.730759, 45.397508, '184 Holland Ave.', 0, 0, 0, 0);
+(1, 'St. James Tennis Club', -75.691462, 45.401809, '183 Third Ave.', 0, 0, 0, 1),
+(2, 'Ottawa Tennis & Lawn Bowling Club', -75.687491, 45.388577, '176 Cameron Ave.', 0, 0, 0, 1),
+(3, 'Rideau Tennis Club', -75.669499, 45.426854, '1 Donald Street', 0, 0, 0, 1),
+(4, 'Ottawa New Edinburgh Club', -75.685965, 45.450308, '504 Rockcliffe Driveway', 0, 0, 0, 1),
+(5, 'Tennis Center West Ottawa', -75.797775, 45.362787, '120 Greenview Ave.', 1, 5, 1, 0),
+(6, 'Carleton Tennis Centre', -75.697517, 45.383532, '1125 Colonel By Dr.', 0, 0, 0, 1),
+(7, 'Dara Tennis Club', -75.548741, 45.288961, '49-A Maple Dr.', 0, 0, 0, 1),
+(8, 'Super Dome', -75.575414, 45.442983, '1660 Bearbrook Rd.', 0, 0, 1, 0),
+(9, 'Elmdale Tennis', -75.730759, 45.397508, '184 Holland Ave.', 0, 0, 0, 1),
+(10, 'Ottawa Athletic Club', -75.613936, 45.40143, '2525 Lancaster Road', 0, 0, 1, 1),
+(11, 'Owl Park', -75.662272, 45.355277, '180 Bourbon St', 0, 0, 0, 1),
+(12, 'Mooney''s Bay Park', -75.690249, 45.366441, '2960 Riverside Dr', 0, 0, 0, 1);
