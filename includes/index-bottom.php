ff0a16fe87274f32cdb1f26948324e8cc4288aa0
Petrus Chan <petchan34@gmail.com>
2012-04-26 23:18:59 -0400

    minified tclocator

--- includes/index-bottom.php
+++ includes/index-bottom.php
@@ -1,6 +1,6 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAq1LN-YdGsIcRgf074Vtwmj2j3GoAbrCo&sensor=false"></script>
 	<script src="js/latlng.min.js"></script>
-	<script src="js/tclocator.js"></script>
+	<script src="js/tclocator.min.js"></script>
 </body>
 </html>
